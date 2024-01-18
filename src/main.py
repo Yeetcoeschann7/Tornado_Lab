a1022f0a84c9a2d677429882833810334a584922
Jacob L <jlewis.cw743@gmail.com>
2024-01-18 15:32:50 -0500

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import asyncio
+import os, os.path
+import tornado.web
+import Index, Quote, TemplateTest
+import os
+
+HTMLDIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..","html"
+    )
+)
+
+def makeApp():
+    endpoints=[
+        ("/",Index.Handler),
+        ("/quote",Quote.Handler),
+        ("/fancy",TemplateTest.Handler)
+    ]
+    app = tornado.web.Application(
+        endpoints,
+        static_path=HTMLDIR
+    )
+    app.listen(8000)
+    print("===========================================")
+    print("Point your browser at http://localhost:8000")
+    print("===========================================")
+    return app
+
+if __name__ == "__main__":
+    app = makeApp()
+    asyncio.get_event_loop().run_forever()   
